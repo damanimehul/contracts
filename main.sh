85e6cba8d6b865956d3d4bbc2211353bd375d298
damanimehul <48509266+damanimehul@users.noreply.github.com>
2023-10-05 19:25:43 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-python runner.py --name "harvest-noise-v1" --config_path "experiment_configs/harvest-noise.json" --w_per_job 16 --workers 80 --mp --gpu 
-python runner.py --name "cleanup-noise-v1" --config_path "experiment_configs/cleanup-noise.json" --w_per_job 16 --workers 80 --mp --gpu 
+python runner.py --name "harvest-noise-v1" --config_path "experiment_configs/harvest-noise.json" --w_per_job 8 --workers 80 --mp --gpu 
+python runner.py --name "cleanup-noise-v1" --config_path "experiment_configs/cleanup-noise.json" --w_per_job 8 --workers 80 --mp --gpu 
