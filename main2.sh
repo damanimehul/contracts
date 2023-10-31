b9f841d88eed52d97d143fe58fba9394958fdc7a
damanimehul <48509266+damanimehul@users.noreply.github.com>
2023-10-31 17:25:36 -0400

    train 2nd stage

--- main2.sh
+++ main2.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python runner.py --name "harvest-noise-nobound-v3" --config_path "experiment_configs/harvest-noise-nobound-v3.json" --w_per_job 14 --workers 84 --mp --gpu --seeds 1 
-CUDA_VISIBLE_DEVICES=1 python runner.py --name "cleanup-noise-nobound-v3" --config_path "experiment_configs/cleanup-noise-nobound-v3.json" --w_per_job 14 --workers 84 --mp --gpu --seeds 1 
+CUDA_VISIBLE_DEVICES=1 python runner.py --name "harvest-noise-nobound-v3" --config_path "experiment_configs/harvest-noise-nobound-v3-stage2.json" --w_per_job 1 --workers 6 --mp --gpu --seeds 1 --load_second_stage "harvest-noise-nobound-v3_2023-10-30"
+CUDA_VISIBLE_DEVICES=1 python runner.py --name "cleanup-noise-nobound-v3" --config_path "experiment_configs/cleanup-noise-nobound-v3-stage2.json" --w_per_job 1 --workers 6 --mp --gpu --seeds 1 --load_second_stage "cleanup-noise-nobound-v3_2023-10-30"
