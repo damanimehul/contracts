5015e6b67923adbb342ec4023203a9ff5c3ba1da
damanimehul <48509266+damanimehul@users.noreply.github.com>
2023-11-11 23:32:08 -0500

    train cleanup for 4 agents

--- main2.sh
+++ main2.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python runner.py --name "harvest-noise-nobound-v3" --config_path "experiment_configs/harvest-noise-nobound-v3-stage2.json" --w_per_job 1 --workers 6 --mp --gpu --seeds 1 --load_second_stage "harvest-noise-nobound-v3_2023-10-30"
-CUDA_VISIBLE_DEVICES=1 python runner.py --name "cleanup-noise-nobound-v3" --config_path "experiment_configs/cleanup-noise-nobound-v3-stage2.json" --w_per_job 1 --workers 6 --mp --gpu --seeds 1 --load_second_stage "cleanup-noise-nobound-v3_2023-10-30"
+CUDA_VISIBLE_DEVICES=1 python runner.py --name "cleanup-noise-nobound-v4" --config_path "experiment_configs/cleanup-noise-nobound-v4.json" --w_per_job 14 --workers 70 --mp --gpu --seeds 5 
+CUDA_VISIBLE_DEVICES=1 python runner.py --name "cleanup-noise-nobound-v4" --config_path "experiment_configs/cleanup-noise-nobound-v4-stage2.json" --w_per_job 1 --workers 5 --mp --gpu --seeds 5 --load_second_stage "cleanup-noise-nobound-v4_2023-11-11"
